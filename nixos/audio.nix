a7bf0979c3c797b32bafdb9f43ce914ca6f8862d
BardofSprites <bard>
2025-09-22 15:57:22 -0400

    add bluetooth support

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -3,11 +3,14 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    wireplumber.enable = true;
 
     extraConfig.pipewire."99-silent-bell.conf" = {
       "context.properties" = {
         "module.x11.bell" = false;
       };
     };
-  };  
+  };
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 }
