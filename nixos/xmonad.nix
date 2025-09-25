eaffd7687fee10ff9ca2ccfc9b54d3af76339ae2
BardofSprites <bard>
2025-09-25 12:27:28 -0400

    XMonad configuration

---
+++ nixos/xmonad.nix
@@ -0,0 +1,29 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.bardConfig.xmonad;
+in {
+  options.bardConfig.xmonad = {
+    enable = mkEnableOption "XMonad desktop";
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver = {
+      enable = true;
+
+      desktopManager = {
+        xterm.enable = false;
+      };
+
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true; # enables xmonad-contrib
+        extraPackages = haskellPackages: with haskellPackages; [
+          xmonad
+          xmonad-contrib
+          xmobar
+        ];
+      };
+    };
+  };
+}
