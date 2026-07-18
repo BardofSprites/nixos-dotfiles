a1430d0cd02176e43f8dc105fb485a1ee81f5c8f
BardofSprites <develop.bard@gmail.com>
2026-07-17 20:16:38 -0400

    shell.nix for running eduroam script

---
+++ uni-network/shell.nix
@@ -0,0 +1,20 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+(pkgs.buildFHSEnv {
+  name = "securew2-fhsenv";
+  targetPkgs =
+    pkgs:
+    (with pkgs; [
+      (python3.withPackages (ps: [ ps.dbus-python ])) # Run embedded Python code
+      coreutils # Needs uname to identify architecture
+      gnutar # Needed to extract emebedded archive
+      xorg.libX11 # for GUI
+      openssl # Required during Python import
+      simpleTpmPk11 # Unknown use
+      which # Used by shell script to find programs
+      xdg-utils # Used by Python script to open links
+      xorg.xwininfo # Unknown use
+    ]);
+  runScript = ./SecureW2_JoinNow.run;
+}).env
